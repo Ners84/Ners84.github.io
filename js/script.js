6dc65dbbf445d8d76f1dd567f4688d2b374614ee
Ners84 <57874061+Ners84@users.noreply.github.com>
2020-01-20 13:32:37 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -160,4 +160,15 @@ $(document).ready(function () {
 
 	new WOW().init();
 
+	function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: -33, lng: 151},
+          disableDefaultUI: true
+        });
+      }
+
+
+
 });
+
